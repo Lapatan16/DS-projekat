5ea29a57d819b56f53a1dc3f1cccda799b224d8d
Mateja Lapatanovic <matejalapatanovic03@gmail.com>
2025-08-11 10:43:40 +0200

    Uradjen designer za backupForm.cs

--- TouristAgencyApp/Forms/BackupForm.cs
+++ TouristAgencyApp/Forms/BackupForm.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 using TouristAgencyApp.Patterns;
 
 namespace TouristAgencyApp.Forms
@@ -11,16 +8,15 @@ namespace TouristAgencyApp.Forms
     {
         public BackupForm(object dbService)
         {
-            this.Text = "Backup";
-            Button btnBackup = new Button { Text = "Backup baze", Dock = DockStyle.Fill };
-            btnBackup.Click += (s, e) =>
-            {
-                string dbFile = "agencija.db";
-                IBackupService backup = new LoggingBackupService(new BackupService(dbFile));
-                backup.CreateBackup();
-                MessageBox.Show("Backup kreiran!");
-            };
-            this.Controls.Add(btnBackup);
+            InitializeComponent();
+        }
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            string dbFile = "agencija.db";
+            IBackupService backup = new LoggingBackupService(new BackupService(dbFile));
+            backup.CreateBackup();
+            MessageBox.Show("Backup kreiran!");
         }
     }
 }
